beb3727f375b4b7824e8108831cd0c61a2af5610
keykibatyr <keykibatyr@gmail.com>
2025-09-30 19:55:57 +0500

    installed chi for router purposes

---
+++ lenslocked/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my Website!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch email me at <a href= \"mailto:keykibatyr@gmail.com\">keykibatyr@gmail.com</a> ")
+}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		w.WriteHeader(404)
+		fmt.Fprintln(w, "page not found")
+	}
+}
+
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	case "/faq":
+// 		faqHandler(w, r)
+// 	default:
+// 		w.WriteHeader(404)
+// 		fmt.Fprintln(w, "page not found")
+// 	}
+// }
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ Page</h1>")
+	fmt.Fprint(w, `<h2><pre>
+Q: Is there a free version?
+A: Yes! We offer a free trial for 30 days on any paid plans.
+
+Q: What are your support hours?
+A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.
+
+Q: How do I contact support?
+A: Email us – <a href="mailto:keykibatyr@gmail.com">support@lenslocked.com</a>
+</pre></h2>`)
+
+}
+
+func main() {
+	var router Router
+	// http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/contact", contactHandler)
+	// http.HandleFunc("/", pathHandler)
+	fmt.Println("Listening to port :7000...")
+	http.ListenAndServe(":7000", router)
+}
