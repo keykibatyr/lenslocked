2bf8049763e9ad57cbfe3868ff7c73bab1a02b4e
keykibatyr <keykibatyr@gmail.com>
2025-10-13 23:47:16 +0500

    added file adding and checkbox

--- lenslocked/controllers/users.go
+++ lenslocked/controllers/users.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 type Users struct {
@@ -16,10 +19,45 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 		Email string
 	}
 	data.Email = r.FormValue("email")
+
 	u.Templates.New.ExecuteTemp(w, data)
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Email: ", r.FormValue("email"))
-	fmt.Fprint(w, "Password: ", r.FormValue("password"))
+	fmt.Fprintln(w, "Email: ", r.FormValue("email"))
+	fmt.Fprintln(w, "Password: ", r.FormValue("password"))
+	fmt.Fprintln(w, "Terms: ", r.FormValue("checkbox1"))
+
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(
+			w,
+			"File has to be <=10mb",
+			http.StatusBadRequest,
+		)
+	}
+
+	file, handler, err := r.FormFile("profile_picture")
+	if err != nil {
+		http.Error(
+			w,
+			"Could not the parse the file",
+			http.StatusInternalServerError,
+		)
+	} else {
+		defer file.Close()
+		log.Print("the file was succesfuly parsed")
+
+		dst, err := os.Create(handler.Filename)
+		if err != nil {
+			log.Print("could not create a file")
+		}
+		defer dst.Close()
+
+		_, err = io.Copy(dst, file)
+		if err != nil {
+			log.Print("could not copy the file to the destination")
+		}
+	}
+	fmt.Fprintf(w, "File uploaded successfully: %s", handler.Filename)
 }
