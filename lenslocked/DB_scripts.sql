0701e3b8f3b727585e809565cad9c77b3186bec9
keykibatyr <keykibatyr@gmail.com>
2025-10-17 19:21:11 +0500

    added db and db related lib

--- lenslocked/DB_scripts.sql
+++ lenslocked/DB_scripts.sql
@@ -6,3 +6,26 @@ last_name TEXT,
 email Text Unique not null
 );
 
+
+CREATE TABLE users(
+id SERIAL PRIMARY KEY
+age INT,
+first_name TEXT,
+last_name TEXT,
+pass_word TEXT,
+email Text Unique not null;
+)
+
+CREATE TABLE tweets(
+id SERIAL PRIMARY KEY,
+user_id INT REFERENCES users(id),
+time_stamp DATE,
+comments INT,
+format TEXT CHECK (format IN ('video', 'image', 'gif'));
+)
+
+CREATE TABLE likes(
+id SERIAL PRIMARY KEY,
+tweet_id INT REFERENCES tweets(id),
+amount INT;
+)
--- lenslocked/cmd/exp/main.go
+++ lenslocked/cmd/exp/main.go
@@ -1,44 +1,148 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-type Address struct{
-	Street string
-	HouseNum int
-} 
-type User struct{
-	Name string
-	Age int
-	Bio string
-	Address *Address
+type PostgresConfig struct{
+	Host string
+	Port string
+	User string
+	Password string
+	Database string
+	SSLMode string
 }
-type UserMeta struct{
-	Visits int
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host = %s  port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, 
+	cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
 }
 
 func main() {
-	t , err := template.ParseFiles("hello.gohtml")
+	cfg := PostgresConfig{
+		Host: "localhost",
+		Port: "5432",
+		User: "baloo",
+		Password: "Alisher2005",
+		Database: "lenslocked",
+		SSLMode: "disable",
+	}
+
+	db, err := sql.Open("pgx", cfg.String())
+
 	if err != nil {
 		panic(err)
 	}
 
-	address := &Address{
-		Street: "Umai Ana",
-		HouseNum: 15,
+	err = db.Ping()
+	if err != nil {
+		panic(err)
 	}
 
-	user := &User{
-		Name: "Alisher",
-		Age: 19,
-		Bio: `<script>alert("ITS PATRICK")</script>`,
-		Address: address,
+	fmt.Println("Connected!")
+	defer db.Close()
+
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS users(
+	 id SERIAL PRIMARY KEY,
+	 name TEXT,
+	 email TEXT UNIQUE NOT NULL
+	 );
+
+	CREATE TABLE IF NOT EXISTS orders(
+	id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL,
+	amount INT,
+	description TEXT);
+	`)
+
+	if err != nil {
+		panic(err)
 	}
 
-	err = t.Execute(os.Stdout, user)
+	fmt.Println("Tables are created!")
+
+	// name := "Alisher"
+	// email := "keykibatyr@gmail.com"
+
+	// _, err = db.Exec(`
+	// INSERT INTO users (name, email)
+	// VALUES($1, $2);`, name, email)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("Info inserted")
+
+	// name = "Bob"
+	// email = "Bob@ver.org"
+	// row := db.QueryRow(`
+	// INSERT INTO users (name, email)
+	// VALUES($1, $2) RETURNING id, name;`, name, email)
+	
+	// var id int 
+	// err = row.Scan(&id, &name)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("User created id:%v, name: %v", id, name)
+
+	// id := 1
+	// row := db.QueryRow(`Select name, email 
+	// From users where id = $1`, id)
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("recieved user; name = %v, email=%v", name, email)
+
+	// userID := 1
+	// for i:=1; i < 6; i++{
+	// 	amount := i * 100
+	// 	desc := fmt.Sprintf("Fake order #%v", i)
+	// 	_, err = db.Exec(`
+	// 	Insert into orders (user_id, description, amount)
+	// 	values($1, $2, $3);`, userID, desc, amount)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// }
+
+	userID := 1
+	type Order struct{
+		Id int
+		UserId int
+		Amount int
+		Description string
+	}
+
+	var objects []Order
+	rows , err := db.Query(`
+	SELECT id, amount, description
+	FROM orders Where user_id = $1`, userID)
+
 	if err != nil{
 		panic(err)
 	}
+
+	defer rows.Close()
+	
+	for rows.Next(){
+		var object Order
+		object.UserId = userID
+		err = rows.Scan(&object.Id, &object.Amount, &object.Description)
+		if err != nil{
+			panic(err)
+		}
+		objects = append(objects, object)
+	}
+
+	fmt.Print(objects)
 }
