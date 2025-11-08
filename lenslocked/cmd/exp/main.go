2e907bc6bcfa34c99cdffd97591389f8a68fcb99
keykibatyr <keykibatyr@gmail.com>
2025-11-08 17:29:36 +0500

    feat: Forgotpassword and send email in exp

--- lenslocked/cmd/exp/main.go
+++ lenslocked/cmd/exp/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/go-mail/mail/v2"
+	"github.com/keykibatyr/lenslocked/models"
 )
 
 const (
@@ -15,26 +14,25 @@ const (
 )
 
 func main() {
-	from := "keykibatyr@gmail.com"
-	to := "alischreck678@gmail.com"
-	subject := "This is my test email"
-	plaintext := "This is the body of email"
-	html := `<h1>HEllo there dude</h1><p>This is the eamil</p><p>Hope u enjoy it</p>`
+	es := models.NewEmailService(models.SMTPConfig{
+		Host: host,
+		Port: port,
+		Username: username,
+		Password: password,
+	})
 
-	msg := mail.NewMessage()
-	msg.SetHeader("From", from)
-	msg.SetHeader("To", to)
-	msg.SetHeader("Subject", subject)
-	msg.SetHeader("text/plain", plaintext)
-	msg.AddAlternative("text/html", html)
-	msg.WriteTo(os.Stdout)
+	email := models.Email{
+		From: "keykibatyr@gmail.com",
+		To: "alischreck678@gmail.com",
+		Subject: "This is my test email",
+		Plaintext: "This is the body of email",
+		HTML: `<h1>Hello there dude</h1><p>This is the eamil</p><p>Hope u enjoy it</p>`,
+	}
 
-	dialer := mail.NewDialer(host, port, username, password)
-	err := dialer.DialAndSend(msg)
+	err := es.Send(email) 
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Print("message sent")
-
 }
