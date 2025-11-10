6f2a48d8ac285e678d12998b7daa394b553a125b
keykibatyr <keykibatyr@gmail.com>
2025-11-10 23:43:33 +0500

    refactored main, added env creds

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/csrf"
+	"github.com/joho/godotenv"
 	"github.com/keykibatyr/lenslocked/controllers"
 	"github.com/keykibatyr/lenslocked/migrations"
 	"github.com/keykibatyr/lenslocked/models"
@@ -18,48 +21,96 @@ type Info struct {
 	Name string
 }
 
+type config struct {
+	PSQL models.PostgresConfig
+	SMTP models.SMTPConfig
+	CSRF struct {
+		Key    string
+		Secure bool
+	}
+	Server struct {
+		Address string
+	}
+}
+
+func loadENVconfig() (config, error) {
+	var cfg config
+	err := godotenv.Load()
+	if err != nil {
+		return cfg, err
+	}
+
+	cfg.PSQL = models.DefaultConfig()
+
+	cfg.CSRF.Key = "qRWLtI8k0q2kZ28nNsG32byMQoqOVmfKOhmLZgv6AD0"
+	cfg.CSRF.Secure = false
+
+	cfg.SMTP.Host = os.Getenv("SMTP_HOST")
+	portSMTP, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		panic(err)
+	}
+	cfg.SMTP.Port = portSMTP
+	cfg.SMTP.Username = os.Getenv("SMTP_USERNAME")
+	cfg.SMTP.Password = os.Getenv("SMTP_PASSWORD")
+
+	cfg.Server.Address = ":3000"
+
+	return cfg, nil
+}
+
 func main() {
-		// Opening the DB
-	cfg := models.DefaultConfig()
-	db, err := models.Open(cfg)
+	cfg, err := loadENVconfig()
+	if err != nil {
+		panic(err)
+	}
+	// Opening the DB
+	db, err := models.Open(cfg.PSQL)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(cfg.String())
+	fmt.Println(cfg.PSQL.String())
 
 	defer db.Close()
 
 	err = models.MigrateFS(db, migrations.FS, ".")
 	if err != nil {
 		panic(err)
-	}	
+	}
 
-	userService := models.UserService{
+	userService := &models.UserService{
 		DB: db,
 	}
-	sessionService := models.SessionService{
+
+	sessionService := &models.SessionService{
 		DB: db,
 	}
+
+	pwResetService := &models.PasswordResetService{
+		DB: db,
+	}
+
+	emailServcie := models.NewEmailService(cfg.SMTP)
 
 
 	umw := controllers.UserMiddleware{
-		SessionService: &sessionService,
+		SessionService: sessionService,
 	}
 
-	csrf_key := "qRWLtI8k0q2kZ28nNsG32byMQoqOVmfKOhmLZgv6AD0"
 	csrfMW := csrf.Protect(
-		[]byte(csrf_key), 
-		csrf.Secure(false), 
+		[]byte(cfg.CSRF.Key),
+		csrf.Secure(cfg.CSRF.Secure),
 		csrf.Path("/"),
 	)
-	
 
 	usersC := controllers.Users{
-		UserService: &userService,
-		SessionService: &sessionService,
+		UserService:    userService,
+		SessionService: sessionService,
+		PasswordResetService: pwResetService,
+		EmailService: emailServcie,
 	}
- 
+
 	usersC.Templates.New = views.Must(views.ParseFileSys(
 		templates.FS, "signup.gohtml", "tailwind.gohtml"))
 
@@ -88,7 +139,6 @@ func main() {
 	faqtpl := views.Must(views.ParseFileSys(
 		templates.FS, "faq.gohtml", "tailwind.gohtml"))
 
-
 	router.Get("/faq", controllers.FAQ(faqtpl))
 
 	router.Get("/signUp", usersC.New)
@@ -98,12 +148,11 @@ func main() {
 	router.Post("/signOut", usersC.ProcessSignOut)
 	router.Get("/forgot-pw", usersC.ForgotPassword)
 	router.Post("/forgot-pw", usersC.ProcessForgotPassword)
-	router.Route("/users/me", func(ro chi.Router){
+	router.Route("/users/me", func(ro chi.Router) {
 		ro.Use(umw.RequireUser)
 		ro.Get("/", usersC.CurrentUser)
 	})
 
-
-	fmt.Println("Listening to port :7000...")
-	http.ListenAndServe(":7000", router)
+	fmt.Printf("Listening to port %s...", cfg.Server.Address)
+	http.ListenAndServe(cfg.Server.Address, router)
 }
